566d910884483731c4f8d377b454a3a0bbb9cfa3
Koppolu Maneesha <165755242+KoppoluManeesha@users.noreply.github.com>
2026-03-17 10:33:04 +0530

    67588

--- orders/models.py
+++ orders/models.py
@@ -13,3 +13,11 @@ class OrderStatus(models.Model):
     name= models.CharField(max_length=50,unique=True)
     def __str__(self):
         return self.name
+class Order(models.Model):
+    status = models.ForeignKey(
+        OrderStatus,
+        on_delete = models.SET_NULL,
+        null=True
+    )
+    def __str__(self):
+        return f"Order {self.id}"
