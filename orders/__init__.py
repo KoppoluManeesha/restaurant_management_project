7d23ed23893f9ccc9970f5c4b5d9ef48543b2ded
Koppolu Maneesha <165755242+KoppoluManeesha@users.noreply.github.com>
2026-03-17 10:28:23 +0530

    67588

---
+++ orders/__init__.py
@@ -0,0 +1,11 @@
+PENDING = "Pending"
+PROCESSING = 'Processing'
+COMPLETED = 'Completed'
+CANCELLED = 'Cancelled'
+
+ORDER_STATUS_CHOICES = [
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    CANCELLED,
+]
